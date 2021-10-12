b77e47275b4a12e0743d1fd458a35b885e0eb0db
kmazia <karla.mazia.1989@gmail.com>
2021-10-12 20:10:08 -0300

    background video e header transparente

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+window.onscroll = () => {
+    hideHeader()
+}
+
+hideHeader = () => {    
+    if (document.documentElement.scrollTop > 100) {
+        document.getElementById('header').style.backgroundColor = '#24263365'
+    } else {        
+        document.getElementById('header').style.backgroundColor = '#242633'
+    }
+}
